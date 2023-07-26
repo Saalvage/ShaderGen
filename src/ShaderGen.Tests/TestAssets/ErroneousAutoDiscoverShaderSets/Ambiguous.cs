5d1948650023a3312cfcdd710d713ccd19ce3cd1
Salvage <29021710+Saalvage@users.noreply.github.com>
2023-07-26 19:04:26 +0200

    Consistency fixes

--- src/ShaderGen.Tests/TestAssets/ErroneousAutoDiscoverShaderSets/Ambiguous.cs
+++ src/ShaderGen.Tests/TestAssets/ErroneousAutoDiscoverShaderSets/Ambiguous.cs
@@ -5,8 +5,8 @@ namespace TestShaders;
 [ShaderClass]
 public class Ambiguous
 {
-    [VertexShader] public void VS() { }
-    [FragmentShader] public void FS() { }
+    [VertexShader] public void VS1() { }
+    [FragmentShader] public void FS1() { }
     [VertexShader] public void VS2() { }
     [FragmentShader] public void FS2() { }
 }
--- src/ShaderGen.Tests/TestAssets/ErroneousAutoDiscoverShaderSets/DuplicateName.cs
+++ src/ShaderGen.Tests/TestAssets/ErroneousAutoDiscoverShaderSets/DuplicateName.cs
@@ -2,12 +2,12 @@
 
 namespace TestShaders;
 
-[ShaderClass("VS", "FS")]
+[ShaderClass("VS1", "FS1")]
 [ShaderClass("VS2", "FS2")]
 public class DuplicateName
 {
-    [VertexShader] public void VS() { }
-    [FragmentShader] public void FS() { }
+    [VertexShader] public void VS1() { }
+    [FragmentShader] public void FS1() { }
     [VertexShader] public void VS2() { }
     [FragmentShader] public void FS2() { }
 }
