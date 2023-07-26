feb626628774ae9f8f89632216451812fdd7c0a7
Salvage <29021710+Saalvage@users.noreply.github.com>
2023-07-26 19:05:45 +0200

    Minor cleanup

--- src/ShaderGen.Primitives/ShaderSetAttribute.cs
+++ src/ShaderGen.Primitives/ShaderSetAttribute.cs
@@ -5,15 +5,8 @@ namespace ShaderGen
     [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
     public class ShaderSetAttribute : Attribute
     {
-        public string Name { get; }
-        public string VertexShader { get; }
-        public string FragmentShader { get; }
-
         public ShaderSetAttribute(string name, string vs, string fs)
         {
-            Name = name;
-            VertexShader = vs;
-            FragmentShader = fs;
         }
     }
 }
